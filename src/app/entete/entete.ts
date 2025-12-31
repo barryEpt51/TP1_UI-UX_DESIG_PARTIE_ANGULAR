9631b3ff5d66cdb88cedab0510f3427e8ef39a90
BARRY <bala.professionnel01@gmail.com>
2025-12-31 10:43:06 +0000

    Créer le composant entête et modifier le selector en entete-cv

---
+++ src/app/entete/entete.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'entete-cv',
+  standalone: true,
+  imports: [],
+  templateUrl: './entete.html',
+  styleUrl: './entete.scss',
+})
+export class Entete {
+
+}
