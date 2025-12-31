2d27052ee1b49556d4163f524b00fa34e36aabdc
BARRY <bala.professionnel01@gmail.com>
2025-12-31 11:14:16 +0000

    Creer la composante formations

--- src/app/app.ts
+++ src/app/app.ts
@@ -3,11 +3,12 @@ import { RouterOutlet } from '@angular/router';
 import { Entete } from "./entete/entete";
 import { ContactCompetencesLangues } from "./contact-competences-langues/contact-competences-langues";
 import { Experiences } from "./experiences/experiences";
+import { Formations } from "./formations/formations";
 
 @Component({
   selector: 'app-root',
   standalone:true,
-  imports: [RouterOutlet, Entete, ContactCompetencesLangues, Experiences],
+  imports: [RouterOutlet, Entete, ContactCompetencesLangues, Experiences, Formations],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
---
+++ src/app/formations/formations.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'formations-cv',
+  standalone : true,
+  imports: [],
+  templateUrl: './formations.html',
+  styleUrl: './formations.scss',
+})
+export class Formations {
+
+}
