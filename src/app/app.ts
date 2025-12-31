0655581a8367e7ee24a7d0a8e80a8546d8b5f6a7
BARRY <bala.professionnel01@gmail.com>
2025-12-31 11:09:28 +0000

    Creer la composante expériences

--- src/app/app.ts
+++ src/app/app.ts
@@ -2,11 +2,12 @@ import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Entete } from "./entete/entete";
 import { ContactCompetencesLangues } from "./contact-competences-langues/contact-competences-langues";
+import { Experiences } from "./experiences/experiences";
 
 @Component({
   selector: 'app-root',
   standalone:true,
-  imports: [RouterOutlet, Entete, ContactCompetencesLangues],
+  imports: [RouterOutlet, Entete, ContactCompetencesLangues, Experiences],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
---
+++ src/app/experiences/experiences.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'experiences-cv',
+  standalone: true,
+  imports: [],
+  templateUrl: './experiences.html',
+  styleUrl: './experiences.scss',
+})
+export class Experiences {
+
+}
